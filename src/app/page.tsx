b9fabf1b51a4c2ebfcb22cb0022ed442cf1abab1
Mayank Saini <mayanksaraswal@gmail.com>
2025-10-19 11:05:27 +0530

    add shadcn ui and databse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+
 const Page =()=>{
   return(
     <div className="text-red-500">
+      <Button variant= "default"> click me </Button>
       Hello World
     </div>
   )
