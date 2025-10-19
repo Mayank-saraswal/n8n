25ac79eb1a3d6b364f9bcfd1030cc618b0545a6a
Mayank Saini <mayanksaraswal@gmail.com>
2025-10-19 11:08:41 +0530

    add shadcn ui and database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button"
+import prisma from "@/lib/db"
 
 
-const Page =()=>{
+const Page = async ()=>{
+  const users = await prisma.user.findMany()
   return(
-    <div className="text-red-500">
-      <Button variant= "default"> click me </Button>
-      Hello World
+    <div className="text-red-500 min-h-screen min-w-screen flex items-center justify-center">
+      {JSON.stringify(users)}
     </div>
   )
 }
