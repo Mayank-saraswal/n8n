072eab100fa24bb8249f70541ec37f835a48800d
Mayank Saini <mayanksaraswal@gmail.com>
2025-10-23 23:00:00 +0530

    add ai providers gemini openai and anthropic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,14 @@ const Page =  ()=>{
     const trpc = useTRPC()
     const QueryClient = useQueryClient()
     const {data} =  useQuery(trpc.getWorkflows.queryOptions());
+
+    const testAi = useMutation(trpc.testAi.mutationOptions(
+      {
+        onSuccess:()=>{
+        toast.success(" Ai Job queued")
+      },
+      }
+    ))
     const create = useMutation(trpc.createWorkflow.mutationOptions({
       onSuccess:()=>{
         toast.success("Job queued")
@@ -25,6 +33,11 @@ const Page =  ()=>{
       <div>
           {JSON.stringify(data , null , 2)}
       </div>
+      <Button  disabled={testAi.isPending} onClick={()=> testAi.mutate()
+
+      }>
+        Test AI
+        </Button>
       <Button disabled={create.isPending}  onClick={()=>create.mutate()}>
         Create Workflow
       </Button>
