c7b7eaa346fff8faf0bf10082aa841a166001228
Mayank Saini <mayanksaraswal@gmail.com>
2025-12-07 21:01:40 +0530

    add something new

--- src/features/executions/components/http-request/executor.ts
+++ src/features/executions/components/http-request/executor.ts
@@ -3,12 +3,19 @@ import { retry } from "@polar-sh/sdk/lib/retries.js";
 import { NonRetriableError } from "inngest";
 import ky ,{type Options as KyOptions } from "ky";
 import { Variable } from "lucide-react";
+import Handlebars from "handlebars";
 
+
+Handlebars.registerHelper("json", (context)=> {
+    const jsonString = JSON.stringify(context , null , 2);
+    const safeString= new Handlebars.SafeString(jsonString)
+    return safeString
+});
 
 type HttpRequestData = {
-    variableName?:string
-    endpoint?: string;
-    method? : "GET" | "POST" | "PUT" | "DELETE" | "PATCH" 
+    variableName:string
+    endpoint: string;
+    method : "GET" | "POST" | "PUT" | "DELETE" | "PATCH" 
     body?:string
 };
 export const httpRequestExecutor:NodeExecutor<HttpRequestData> = async({
@@ -28,13 +35,25 @@ export const httpRequestExecutor:NodeExecutor<HttpRequestData> = async({
         throw new NonRetriableError("Variable name not configured ");
     }
 
+     if(!data.method){
+
+        throw new NonRetriableError("method not configured ");
+    }
+
+
     const result = await step.run("http-request" , async()=>{
-        const endpoint = data.endpoint!
-        const method = data.method || "GET"
+        const endpoint = Handlebars.compile(data.endpoint)(context);
+        console.log("ENDPOINT   ",endpoint);
+        const method = data.method 
         const options: KyOptions ={method};
         if(["POST","PUT","PATCH"].includes(method)){
+            const resolved = Handlebars.compile(data.body || "{}")(context);
+            JSON.parse(resolved)
             
-                options.body = data.body
+                options.body = resolved;
+                options.headers = {
+                    "Content-Type":"application/json",
+                }
             
         }
         const responce = await ky(endpoint, options);
@@ -48,16 +67,13 @@ export const httpRequestExecutor:NodeExecutor<HttpRequestData> = async({
             },
         }
 
-        if(data.variableName){
+        
              return {
             ...context,
             [data.variableName]:responsePayload
         }
-        }
-           return {
-            ...context,
-            ...responsePayload
-           }
+        
+          
        
 
     })
